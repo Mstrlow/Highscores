6aa499ab4e46638f6c941c1bc402b833a6dddd2a
Tobias <mstrl0w@web.de>
2015-10-20 10:18:28 +0200

    fixing js

--- newest.php
+++ newest.php
@@ -80,5 +80,7 @@
         </div>
       </div>
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="/js/b.m.js"></script>
   </body>
 </html>
